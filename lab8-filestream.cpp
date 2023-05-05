bc85e34a5ebf6050cce575a62a1dff31e18fa3fc
swapfr <97851724+swapfr@users.noreply.github.com>
2023-05-05 15:02:22 +0300

    Update lab8-filestream.cpp

--- lab8-filestream.cpp
+++ lab8-filestream.cpp
@@ -1,165 +1,147 @@
-import bpy
-from bpy_extras.io_utils import ImportHelper
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <string>
 
+using namespace std;
 
-print("====" * 30)
+void create_2d_array_and_write_it_to_the_file(const int ROWS, const int COLS, string &filename);
+void show_2d_array(const vector<vector<int>>& _arr);
+void write_array_to_the_file(const vector<vector<int>>& _arr, const int ROWS, const int COLS, const string& filename);
 
+vector<vector<int>> read_array_from_the_file(const string& file);
 
+int main()
+{
 
-
-def menu_func_import(self, context):
-    self.layout.operator(import_ipl_file.bl_idname, text = 'IPL Import')
-
-
-class import_ipl_file(bpy.types.Operator, ImportHelper): 
-    bl_idname = "object.custom_draw" 
-    bl_label = "Import" 
-  
+    system("chcp 1251 > nul");
     
-    filepath = bpy.props.StringProperty(subtype = "FILE_PATH") 
+    srand(time(NULL));
     
-    instancing_check1 : bpy.props.BoolProperty(name = 'Import as instancing', description = 'Description', default = False)
-    filter_glob : bpy.props.StringProperty(default='*.ipl', options={'HIDDEN'})
+    int rows, cols;;
+    
 
-    def execute(self, context): 
-        instancing_check = not (self.instancing_check1)
+    cout << "кол-во строк = "; cin >> rows;
+    cout << "кол-во колонок = "; cin >> cols;
+
+    string file_name = "input.txt";
+    string result_file_name = "result.txt";
+
+    create_2d_array_and_write_it_to_the_file(rows, cols, file_name);
+
+    vector<vector<int>> A = read_array_from_the_file(file_name);
+
+    if (A.size() > 1) {
+
+        show_2d_array(A);
+
+        //Варіант 3 В масиві А знайти значення максимального елементу і
+        //замінити ним кожен елемент в другому стовпці
+
+
+        int max_var = A[0][0];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (A[i][j] > max_var) {
+                    max_var = A[i][j];
+                }
+            }
+        }
+
+        cout << max_var << endl;
+
+        for (int i = 0; i < rows; i++) {
+            A[i][2] = max_var;
+        }
+
+        write_array_to_the_file(A, rows, cols, result_file_name);
+
+    }
+    return 0;
+}
+
+
+
+void create_2d_array_and_write_it_to_the_file(const int ROWS, const int COLS, string& filename) {
+
+    vector<vector<int>> arr(ROWS, vector<int>(COLS));
+    
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            arr[i][j] = rand() % 40 - 20;
+        }
+    }
+
+    ofstream outfile(filename);
+
+    outfile << ROWS << endl;
+    outfile << COLS << endl;
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            outfile << arr[i][j] << " ";
+        }
+        outfile << endl;
+    }
+    outfile.close();
+
+}
+
+
+vector<vector<int>> read_array_from_the_file(const string& file) {
+
+    ifstream infile(file);
+    
+    if (!infile.is_open()) {
         
-        print(self.filepath) 
-        
-        print("\n\n")
+        cout << "файла нет" << endl;
 
-        encoding = "cp1251" 
-        ipl_path = self.filepath 
- 
-        ipl_objects = {} 
-        
-        #selecting active collection to the root "scene collection" to avoid some problems with the instancing
-        bpy.context.view_layer.active_layer_collection = bpy.context.view_layer.layer_collection
- 
-        with open(ipl_path, "r", encoding=encoding) as f: 
-            for line in f: 
-                if line.strip() == "end":
-                    break
-                
-                elements = [elem.strip() for elem in line.split(",")] 
-                
-                if len(elements) == 11: 
-                    
-                    obj_name = elements[1]
-                    model_id = int(elements[0])
-                    
-                    x, y, z, rotx, roty, rotz, rotw = map(float, elements[3:10]) 
-                    
-                    if obj_name not in ipl_objects:
-                        ipl_objects[obj_name] = {"coords_rot_list": [], "check": 0}
-                    
-                    ipl_objects[obj_name]["coords_rot_list"].append((x, y, z, rotx, roty, rotz, rotw, model_id))
-                    ipl_objects[obj_name]["check"] += 1
-        
-        for obj_name, data in ipl_objects.items():
-            obj = bpy.data.objects.get(obj_name)
-        
-            if obj is None:
-                #print(f" {obj_name} not found in the scene.")
-                continue
-            
-            obj_location = bpy.data.objects[obj_name].location
-            
-            if obj_location != [0.0, 0.0, 0.0]:
-                obj.location = (0.0, 0.0, 0.0)
-            
-            coords_rot_list = data['coords_rot_list']
-            check = data['check']
-            
-            bpy.data.objects[obj_name]['id'] = coords_rot_list[0][7]     #model id
-            
-            
-            if check > 1 and instancing_check == True:
-               
-                collection_name = obj_name + "_collection"
-             
-                if collection_name not in bpy.data.collections:
-                    
-                    new_collection = bpy.data.collections.new(collection_name)
-                    bpy.context.scene.collection.children.link(new_collection)
-                    
-                for i, coords_rot in enumerate(coords_rot_list): 
-                    
-                    existing_obj = None             #   if an object with the coords from ipl already 
-                    for o in bpy.data.objects:      #   exist in the scene - it won't be copied
+        return {{0}};
+    }
 
-                        obj_name_check = o.name
-                        obj_coord_check = [float(f"{coord:.1f}") for coord in (o.location)]
-                        coords_list_check = [float(f"{coord:.1f}") for coord in coords_rot[0:3]]
+    int rows, cols;
 
-                        if "." in o.name:
-                            obj_name_check = o.name.split(".")[0]
+    infile >> rows;
+    infile >> cols;
 
-                        if obj_name_check == obj_name and obj_coord_check == coords_list_check:
-                            existing_obj = o
-                            #print("match found")
-                    
-                    if existing_obj is None:
-                        
-                        new_obj = obj.copy()
-                        new_obj.data = obj.data.copy()
-                        new_obj.location = coords_rot[0:3] 
-                        new_obj.rotation_mode = 'QUATERNION'
-                        new_obj.rotation_quaternion = (-(coords_rot[6]), coords_rot[3], coords_rot[4], coords_rot[5])
-                        new_obj.name = f"{obj_name}.0{i}"
-                        bpy.data.collections[collection_name].objects.link(new_obj)
-                    
-                        #print(f"{new_obj.name} location: {new_obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                        #print("\n")
-                    
-            elif check == 1:
-               
-                coords_rot = coords_rot_list[0]
-                obj.location = coords_rot[0:3] 
-                obj.rotation_mode = 'QUATERNION'
-                obj.rotation_quaternion = (-(coords_rot[6]), coords_rot[3], coords_rot[4], coords_rot[5])
-                
-                #print(f"{obj_name} location: {obj.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                #print("\n")
-            else:
-                
-                collection_name = obj_name + "_collection_inst"
-                
-                if collection_name not in bpy.data.collections:
-                    
-                    new_collection = bpy.data.collections.new(collection_name)
-                    bpy.context.scene.collection.children.link(new_collection)
-                    
-                for i, coords_rot in enumerate(coords_rot_list):
-                    
-                    existing_obj = None
-                    for o in bpy.data.objects:
+    vector<vector<int>> arr(rows, vector<int>(cols));
 
-                        obj_name_check = o.name
-                        obj_coord_check = [float(f"{coord:.1f}") for coord in (o.location)]
-                        coords_list_check = [float(f"{coord:.1f}") for coord in coords_rot[0:3]]
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            infile >> arr[i][j];
+        }
+    }
 
-                        if "." in o.name:
-                            obj_name_check = o.name.split(".")[0]
+    infile.close();
 
-                        if obj_name_check == obj_name and obj_coord_check == coords_list_check:
-                            existing_obj = o
+    return arr;
 
-                    if existing_obj is None:
+}
 
-                        bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0.0, 0.0, 0.0), scale=(1, 1, 1))                   
-                        obj_inst = bpy.context.object
-                        obj_inst.name = f"{obj_name}.0{i}"
-                        obj_inst.rotation_mode = 'QUATERNION'
-                        obj_inst.rotation_quaternion = (-(coords_rot[6]), coords_rot[3], coords_rot[4], coords_rot[5])
-                        obj_inst.location = coords_rot[0:3] 
-                        bpy.data.collections[collection_name].objects.link(obj_inst)
-                        obj_inst.instance_type = 'COLLECTION'
-                        obj_inst.instance_collection = bpy.data.collections[obj_name + ".dff"]
-                    
-                        #print(f"{obj_inst.name} location: {obj_inst.location} rotation: (x = {coords_rot[3]}, y = {coords_rot[4]}, z = {coords_rot[5]}, w = {coords_rot[6]})")
-                        #print("\n")
-                    
-        print ("Import magic happend!\n")
 
-        return {'FINISHED'} 
+void show_2d_array(const vector<vector<int>>& _arr) {
+
+    cout << endl << endl; for (int i = 0; i < _arr.size(); i++) {
+        for (int j = 0; j < _arr[i].size(); j++) {cout << _arr[i][j] << "\t| "; }
+        cout << endl;}
+cout << endl << endl;}
+
+
+
+void write_array_to_the_file(const vector<vector<int>>& _arr, const int ROWS, const int COLS, const string& filename) {
+
+    ofstream outfile(filename);
+
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            outfile << _arr[i][j] << " ";
+        }
+        outfile << endl;
+    }
+
+    outfile.close();
+}
